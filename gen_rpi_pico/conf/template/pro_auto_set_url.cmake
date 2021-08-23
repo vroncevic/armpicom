928f26c3ab3294a7051a2f44d5e9f4c05855f39d
Vladimir Roncevic <elektron.ronca@gmail.com>
2021-08-23 23:51:56 +0200

    added draft generator with template

---
+++ gen_rpi_pico/conf/template/pro_auto_set_url.cmake
@@ -0,0 +1,15 @@
+#
+# @brief   ${PRO} Setup project url
+# @version v1.0.0
+# @date    ${DATE}
+# @author  Vladimir Roncevic <elektron.ronca@gmail.com>
+#
+
+# Macro for setup project url
+macro(pro_auto_set_url TARGET)
+    file(
+        RELATIVE_PATH URL_REL_PATH "${PRO_PATH}" "${CMAKE_CURRENT_LIST_DIR}"
+    )
+    pico_set_program_url(${TARGET} "${URL_REL_PATH}")
+endmacro()
+
